e8f38f97115de7a6b547e532a294617d3af2d45e
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2023-07-22 21:35:10 +0200

    added daisyUI for reusable UI components built on tailwind. added dark- light-mode switch

--- src/core/components/MainNavbar.tsx
+++ src/core/components/MainNavbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { Routes } from "@blitzjs/next"
+import React from "react"
 
 const MainNavbar = () => {
   let tabs = [
@@ -13,9 +14,22 @@ const MainNavbar = () => {
     },
   ]
 
+  const [theme, setTheme] = React.useState("light")
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+  React.useEffect(() => {
+    document.querySelector("html").setAttribute("data-theme", theme)
+  }, [theme])
+
   return (
     <div className="sticky top-0">
       <nav className="flex space-x-8" aria-label="Tabs">
+        <label className="swap swap-rotate">
+          <input onClick={toggleTheme} type="checkbox" />
+          <div className="swap-on">Dark</div>
+          <div className="swap-off">Light</div>
+        </label>
         {tabs.map((tab) => (
           <Link key={tab.name} href={tab.href}>
             {tab.name}
--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import React, { FC } from "react"
+import React from "react"
 import { BlitzLayout } from "@blitzjs/next"
 import MainNavbar from "../components/MainNavbar"
 
