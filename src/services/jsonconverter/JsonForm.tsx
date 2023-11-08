5cdea921b11b282a008b7c16483805526209cbb4
engerst <dibgray@gmail.com>
2023-11-08 10:02:26 -0500

    added cleanning function to remove unwanted chars from json

--- src/services/jsonconverter/JsonForm.tsx
+++ src/services/jsonconverter/JsonForm.tsx
@@ -1,20 +1,13 @@
 import React from "react"
-import { RJSFSchema } from "@rjsf/utils"
 import validator from "@rjsf/validator-ajv8"
-import Form from "@rjsf/core"
 import { withTheme, ThemeProps } from "@rjsf/core"
 import DaisyTheme from "./DaisyTheme"
-import testJson2 from "./testjson2.js"
-
-// const schema: RJSFSchema = JSON.parse(testJson2)
-
-const log = (type) => console.log.bind(console, type)
 
 const ThemedForm = withTheme(DaisyTheme)
 const JsonForm = (props) => {
-  const [schema, rest] = [...props]
-  log(rest)
-  return <div>{<ThemedForm schema={schema} validator={validator} {...props} />}</div>
+  const { schema, ...rest } = props
+
+  return <div>{<ThemedForm schema={schema} validator={validator} {...rest} />}</div>
 }
 
 export default JsonForm
