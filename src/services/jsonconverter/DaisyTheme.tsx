eec13f6389745ea31d6192b64fd816d109b85675
The Doom Lab <buchananlab@gmail.com>
2024-01-31 09:59:46 -0500

    SMALL PROGRESS

--- src/services/jsonconverter/DaisyTheme.tsx
+++ src/services/jsonconverter/DaisyTheme.tsx
@@ -18,20 +18,68 @@ function MyTitleField<
 >(props: TitleFieldProps<T, S, F>) {
   const { id, title, required } = props
   return (
-    <legend id={id} className="text-2xl">
-      <div className="text-2xl">{title}</div>
+    <legend id={id} className="text-2xl erin">
+      <div className="text-2xl erin">{title}</div>
       {required && <span className="required">{REQUIRED_FIELD_SYMBOL}</span>}
     </legend>
   )
 }
 
-//update the template
-const myTemplates: RegistryTemplatesType = {
-  TitleFieldTemplate: MyTitleField,
+// this is the description
+import { DescriptionFieldProps, FormContextType, RJSFSchema, StrictRJSFSchema } from "@rjsf/utils"
+function MyDescriptionField<
+  T = any,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = any
+>(props: DescriptionFieldProps<T, S, F>) {
+  const { id, description } = props
+  if (!description) {
+    return null
+  }
+  if (typeof description === "string") {
+    return (
+      <p id={id} className="text-2xl marton">
+        {description}
+      </p>
+    )
+  } else {
+    return (
+      <div id={id} className="text-2xl marton">
+        {description}
+      </div>
+    )
+  }
 }
 
+// here's the custom widgets
+const MyTextWidget = (props: WidgetProps) => {
+  return (
+    <div className="flex">
+      <input
+        type="text"
+        style={{ fontSize: "1rem" }}
+        className="input input-bordered m-2 w-full max-w-xs font-serif"
+        value={props.value || ""}
+        required={props.required}
+        onChange={(event) => props.onChange(event.target.value)}
+      />
+    </div>
+  )
+}
+
+//create Registry information
+const myTemplates: RegistryTemplatesType = {
+  StringField: MyTitleField,
+  DescriptionFieldTemplate: MyDescriptionField,
+}
+
+const myWidgets: RegistryWidgetsType = {
+  TextWidget: MyTextWidget,
+}
+
+// create the overall theme to use on the other page
 const DaisyTheme: ThemeProps = {
-  // widgets: myWidgets,
+  widgets: myWidgets,
   templates: myTemplates,
 }
 export default DaisyTheme
