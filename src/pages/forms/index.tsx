e3a497e5a0a4137d467ecd5146bc3a80e1bf02e8
The Doom Lab <buchananlab@gmail.com>
2024-03-06 13:59:38 -0600

    updating tiny stuff

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -7,15 +7,19 @@ import Layout from "src/core/layouts/Layout"
 import { HomeSidebarItems } from "src/core/layouts/SidebarItems"
 import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 
-import React from "react"
+import React, { useRef } from "react"
 import ReactDOM from "react-dom"
 import { ReactFormBuilder } from "staple-form-builder"
-import { DemoBar } from "staple-form-builder"
 import "staple-form-builder/dist/app.css"
 
 const FormBuilderPage = () => {
   const sidebarItems = HomeSidebarItems("Forms")
-
+  const formBuilderRef = useRef()
+  const handleSave = () => {
+    // Access the built JSON form data
+    const formData = formBuilderRef.current.getData()
+    console.log("Built JSON Form Data:", formData)
+  }
   return (
     <Layout sidebarItems={sidebarItems} sidebarTitle="Home">
       <Head>
@@ -24,8 +28,8 @@ const FormBuilderPage = () => {
 
       <main className="flex flex-col mt-2 mx-auto w-full max-w-7xl">
         <Suspense fallback={<div>Loading...</div>}>
-          <DemoBar />
-          <ReactFormBuilder />
+          <ReactFormBuilder ref={formBuilderRef} />
+          <button onClick={handleSave}>Save Form Data</button>
         </Suspense>
       </main>
     </Layout>
