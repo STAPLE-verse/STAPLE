be1276dc9e767d8aa85c59cc3e3b349a44199525
Chris Hartgerink <chris@libscie.org>
2024-04-10 18:06:12 +0200

    Add example form builder

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -1,4 +1,6 @@
-import { Suspense } from "react"
+"use client"
+
+import { Suspense, useEffect, useState } from "react"
 import Head from "next/head"
 import { usePaginatedQuery } from "@blitzjs/rpc"
 import { useRouter } from "next/router"
@@ -10,16 +12,43 @@ import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import React, { useRef } from "react"
 import ReactDOM from "react-dom"
 import { ReactFormBuilder } from "staple-form-builder"
-import "staple-form-builder/dist/app.css"
+import { FormBuilder, PredefinedGallery } from "@ginkgo-bioworks/react-json-schema-form-builder"
+// import "staple-form-builder/dist/app.css"
+
+const Example = () => {
+  const [state, setState] = useState({ schema: "{}", uischema: "{}" })
+  const [render, setRender] = useState(false)
+
+  useEffect(() => {
+    if (!render) {
+      setRender(true)
+    }
+
+    console.log(state.schema)
+  }, [])
+
+  // TODO: This logs a lot - remove
+  useEffect(() => {
+    console.log(state.schema)
+  })
+
+  return render ? (
+    <FormBuilder
+      schema={state.schema}
+      uischema={state.uischema}
+      mods={{}}
+      onChange={(newSchema, newUiSchema) => {
+        setState({
+          schema: newSchema,
+          uischema: newUiSchema,
+        })
+      }}
+    />
+  ) : null
+}
 
 const FormBuilderPage = () => {
   const sidebarItems = HomeSidebarItems("Forms")
-  const formBuilderRef = useRef()
-  const handleSave = () => {
-    // Access the built JSON form data
-    const formData = formBuilderRef.current.getData()
-    console.log("Built JSON Form Data:", formData)
-  }
   return (
     <Layout sidebarItems={sidebarItems} sidebarTitle="Home">
       <Head>
@@ -28,8 +57,7 @@ const FormBuilderPage = () => {
 
       <main className="flex flex-col mt-2 mx-auto w-full max-w-7xl">
         <Suspense fallback={<div>Loading...</div>}>
-          <ReactFormBuilder ref={formBuilderRef} />
-          <button onClick={handleSave}>Save Form Data</button>
+          <Example />
         </Suspense>
       </main>
     </Layout>
