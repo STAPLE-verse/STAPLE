a5c7ae94fdbaa38c93e577ccf45eee4261465c04
The Doom Lab <buchananlab@gmail.com>
2024-04-12 21:08:47 -0400

    notes for newest updates

--- src/pages/labels/index.tsx
+++ src/pages/labels/index.tsx
@@ -24,6 +24,13 @@ const LabelBuilderPage = () => {
       <main className="flex flex-col mt-2 mx-auto w-full max-w-7xl">
         <Suspense fallback={<div>Loading...</div>}>
           Label creation and updating will go here
+          <br />
+          - make this like the task table - where you can view all Labels with a view button that
+          opens a modal that allows you to edit or delete the label in case of misspellings
+          <br />
+          - at the bottom of the paged table have a "create" button that opens a modal that allows
+          you to add a new Label
+          <br />- new label page should have string fields for name, description, and taxonomy
         </Suspense>
       </main>
     </Layout>
