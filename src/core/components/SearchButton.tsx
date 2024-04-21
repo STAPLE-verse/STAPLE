7895a2cf59f6901e01415140284ef4845674b758
The Doom Lab <buchananlab@gmail.com>
2024-04-21 15:58:22 -0400

    dashboard and search bar updates

--- src/core/components/SearchButton.tsx
+++ src/core/components/SearchButton.tsx
@@ -19,8 +19,8 @@ const SearchButton = ({ onChange, debounceTime = 500 }: Props) => {
   return (
     <div>
       <div
-        className="flex flex-row  py-3 px-6 mx-auto w-full max-w-md items-center  justify-between relative text-gray-400 focus-within:text-gray-600
-      bg-gray-50 border-b
+        className="flex flex-row  py-3 px-6 mx-auto w-full max-w-md items-center justify-between relative text-base-400 focus-within:text-base-600
+      bg-base-50 border-b
       "
       >
         <MagnifyingGlassIcon className="w-5 h-5 absolute ml-3 pointer-events-none"></MagnifyingGlassIcon>
@@ -30,9 +30,9 @@ const SearchButton = ({ onChange, debounceTime = 500 }: Props) => {
           debounce={debounceTime}
           value={currentSearchTerm}
           onChange={handleSearch}
-          placeholder=" Search project"
-          className="pr-3 pl-10 py-2 w-full h-10 w-full pr-3 pl-10  font-semibold placeholder-gray-500 text-black rounded-2xl border-none
-          ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
+          placeholder="Search Projects"
+          className="pr-3 pl-10 py-2 w-full h-10 w-full pr-3 pl-10  font-semibold placeholder-base-500 text-black rounded-2xl border-none
+          ring-2 ring-base-300 focus:ring-base-500 focus:ring-2"
         />
       </div>
     </div>
