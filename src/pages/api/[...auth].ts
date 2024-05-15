2ed4e99652b72f1346203484f9984b13436032f6
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-05-15 20:28:59 +0200

    orcid?

---
+++ src/pages/api/[...auth].ts
@@ -0,0 +1,39 @@
+import { passportAuth } from "@blitzjs/auth"
+import { Strategy as OrcidStrategy } from "passport-orcid"
+import db from "db"
+import { api } from "src/blitz-server"
+
+export default api(
+  passportAuth(({ ctx, req, res }) => ({
+    successRedirectUrl: "/main",
+    errorRedirectUrl: "/main",
+    strategies: [
+      {
+        strategy: new OrcidStrategy(
+          {
+            sandbox: process.env.ORCID_CLIENT_SANDBOX === "true",
+            clientID: process.env.ORCID_CLIENT_ID,
+            clientSecret: process.env.ORCID_CLIENT_SECRET,
+            callbackURL: `${process.env.APP_ORIGIN}/api/auth/orcid/callback`,
+          },
+          async function (accessToken, refreshToken, params, profile, done) {
+            try {
+              const user = await db.user.update({
+                where: {
+                  id: ctx.session.$publicData.userId!,
+                },
+                data: {
+                  orcid: params.orcid,
+                },
+              })
+            } catch (e) {
+              console.log(e)
+            }
+
+            return done(null, { publicData: ctx.session.$publicData })
+          }
+        ),
+      },
+    ],
+  }))
+)
--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -14,6 +14,7 @@ import { FormProfileSchema } from "src/users/schemas"
 import { Routes } from "@blitzjs/next"
 import logout from "src/auth/mutations/logout"
 import { HomeSidebarItems } from "src/core/layouts/SidebarItems"
+import Link from "next/link"
 
 export function ProfileForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
@@ -102,6 +103,11 @@ export const EditProfile = () => {
               }
             }}
           />
+          <Link href="/api/auth/orcid" legacyBehavior>
+            <button className="mb-1 flex rounded-md bg-emerald-50 py-2 px-4 text-sm font-medium text-emerald-700 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-0 dark:border dark:border-gray-600 dark:bg-gray-800 dark:text-emerald-500 dark:hover:border-gray-400 dark:hover:bg-gray-700">
+              Connect your ORCID
+            </button>
+          </Link>
 
           <div className="flex justify-end mt-4">
             <button
