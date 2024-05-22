610d44c6afa6e5fca00641a5d3f7e34ad6bc56e5
The Doom Lab <buchananlab@gmail.com>
2024-05-22 14:08:35 -0400

    project notifications spot added

--- src/pages/projects/[projectId]/notifications/index.tsx
+++ src/pages/projects/[projectId]/notifications/index.tsx
@@ -3,12 +3,17 @@ import Head from "next/head"
 import Layout from "src/core/layouts/Layout"
 import { ProjectSidebarItems } from "src/core/layouts/SidebarItems"
 import { ProjectNotificationList } from "src/messages/components/ProjectNotificationList"
+import { useQuery } from "@blitzjs/rpc"
+import getProject from "src/projects/queries/getProject"
+import { useParam } from "@blitzjs/next"
 
-const ProjectNotificationsPage = ({ projectId: projectId }) => {
-  const sidebarItems = ProjectSidebarItems("Notifications")
+const ProjectNotificationsPage = () => {
+  const projectId = useParam("projectId", "number")
+  const [project] = useQuery(getProject, { id: projectId })
+  const sidebarItems = ProjectSidebarItems(projectId!, "Notifications")
 
   return (
-    <Layout sidebarItems={sidebarItems} sidebarTitle="Home">
+    <Layout sidebarItems={sidebarItems} sidebarTitle={project.name}>
       <Head>
         <title>Project Notifications</title>
       </Head>
