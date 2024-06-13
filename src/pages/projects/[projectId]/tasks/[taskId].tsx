df225d6fe7f6c5e098c8d9b19a084a7bc302ac81
The Doom Lab <buchananlab@gmail.com>
2024-06-13 15:26:56 -0400

    this page merged wrong across all pulls fixed

--- src/pages/projects/[projectId]/tasks/[taskId].tsx
+++ src/pages/projects/[projectId]/tasks/[taskId].tsx
@@ -7,12 +7,9 @@ import { TaskSummary } from "src/tasks/components/TaskSummary"
 import { Suspense, useContext } from "react"
 import Head from "next/head"
 import Layout from "src/core/layouts/Layout"
-import getTask from "src/tasks/queries/getTask"
-import deleteTask from "src/tasks/mutations/deleteTask"
-import JsonForm from "src/assignments/components/JsonForm"
-
-import getJsonSchema from "src/services/jsonconverter/getJsonSchema"
-import Modal from "src/core/components/Modal"
+import { useParam } from "@blitzjs/next"
+import { useQuery } from "@blitzjs/rpc"
+import getProject from "src/projects/queries/getProject"
 import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import getContributor from "src/contributors/queries/getContributor"
 import { ContributorPrivileges } from "@prisma/client"
@@ -20,12 +17,42 @@ import { TaskInformation } from "src/tasks/components/TaskInformation"
 import { AssignmentCompletion } from "src/assignments/components/AssignmentCompletion"
 import { TaskProvider, TaskContext } from "src/tasks/components/TaskContext"
 
+const TaskContent = () => {
+  const projectId = useParam("projectId", "number")
+
+  const taskContext = useContext(TaskContext)
+  const { task } = taskContext
+  // TODO: replace by hook
+  const currentUser = useCurrentUser()
+  const [currentContributor] = useQuery(getContributor, {
+    where: { projectId: projectId, userId: currentUser!.id },
+  })
+
+  if (!task) {
+    return <div>Loading...</div>
+  }
+  // return (
+  //   {!!task && <></>}
+  // )
+  return (
+    <>
+      <Head>
+        <title>Task {task.name}</title>
+      </Head>
+      <main className="flex flex-col mb-2 mt-2 mx-auto w-full max-w-7xl">
+        <div className="flex flex-row justify-center m-2">
+          <TaskInformation />
+          <AssignmentCompletion />
+        </div>
+        {currentContributor.privilege == ContributorPrivileges.PROJECT_MANAGER && <TaskSummary />}
+      </main>
+    </>
+  )
+}
+
 // show the task page
 export const ShowTaskPage = () => {
-  const projectId = useParam("projectId", "number")
-  const [project] = useQuery(getProject, { id: projectId })
   const taskId = useParam("taskId", "number")
-
   // return the page
   return (
     <Layout>
