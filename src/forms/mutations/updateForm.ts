00129baedb25b3dfa01113ea5022109da1c97c64
enyeal <dibgray@gmail.com>
2024-06-30 11:40:24 +0200

    fix form update not null accept

--- src/forms/mutations/updateForm.ts
+++ src/forms/mutations/updateForm.ts
@@ -1,15 +1,19 @@
-// @ts-nocheck
-// null not assignable to json null
-
 import { resolver } from "@blitzjs/rpc"
-import db from "db"
+import db, { Prisma } from "db"
 import { EditFormSchema } from "../schemas"
 
 export default resolver.pipe(
   resolver.zod(EditFormSchema),
   resolver.authorize(),
   async ({ id, ...data }) => {
-    const forms = await db.forms.update({ where: { id }, data })
+    const newSchema = data.schema != null ? data.schema : Prisma.JsonNull
+    const forms = await db.forms.update({
+      where: { id },
+      data: {
+        uiSchema: data.uiSchema,
+        schema: newSchema,
+      },
+    })
 
     return forms
   }
