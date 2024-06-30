5b1e832c0fdd670a77ebd50f6e154ee60776e531
enyeal <dibgray@gmail.com>
2024-06-30 11:13:16 +0200

    fix schema no accepting null

--- src/forms/mutations/createForm.ts
+++ src/forms/mutations/createForm.ts
@@ -1,16 +1,15 @@
-// @ts-nocheck
-// issue with schema line?
 import { resolver } from "@blitzjs/rpc"
 import { CreateFormSchema } from "../schemas"
-import db from "db"
+import db, { Prisma } from "db"
 
 export default resolver.pipe(
   resolver.zod(CreateFormSchema),
   resolver.authorize(),
   async ({ userId, schema, uiSchema }) => {
+    const newSchema = schema != null ? schema : Prisma.JsonNull
     const form = await db.forms.create({
       data: {
-        schema,
+        schema: newSchema,
         uiSchema,
         user: {
           connect: { id: userId },
