21bb5e2657817f276da8fbee90360db255b48e9d
The Doom Lab <buchananlab@gmail.com>
2024-08-26 11:23:12 -0400

    only one centered

--- src/projects/components/ProjectForm.tsx
+++ src/projects/components/ProjectForm.tsx
@@ -11,8 +11,8 @@ export function ProjectForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
     <Form<S> {...props}>
       <LabeledTextField
         name="name"
-        label="Name"
-        placeholder="Name:"
+        label="Name: (Required)"
+        placeholder="Name"
         type="text"
         className="mb-4 text-primary w-1/2 border-primary border-2 bg-base-300"
       />
@@ -41,23 +41,23 @@ export function ProjectForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
       <br />
       <LabeledTextField
         name="citation"
-        label="Citation"
-        placeholder="Citation:"
+        label="Citation:"
+        placeholder="Citation"
         type="text"
         className="mb-4 text-primary w-1/2 border-primary border-2 bg-base-300"
       />
       <br />
       <LabeledTextField
         name="publisher"
-        label="Publisher"
-        placeholder="Publisher:"
+        label="Publisher:"
+        placeholder="Publisher"
         type="text"
         className="mb-4 text-primary w-1/2 border-primary border-2 bg-base-300"
       />
       <br />
       <LabeledTextField
-        name="identifier"
-        label="Identifier:"
+        name="identifier:"
+        label="Identifier"
         placeholder="Identifier: DOI, ISBN, etc."
         type="text"
         className="mb-4 text-primary w-1/2 border-primary border-2 bg-base-300"
