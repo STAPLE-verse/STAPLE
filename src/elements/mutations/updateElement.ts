d4d09a1e2ac0c70c0d86f48a251861ccbd654264
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-08-28 06:28:37 +0200

    made element.description optional

--- src/elements/mutations/updateElement.ts
+++ src/elements/mutations/updateElement.ts
@@ -1,14 +1,14 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { UpdateElementSchema } from "../schemas";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { UpdateElementSchema } from "../schemas"
 
 export default resolver.pipe(
   resolver.zod(UpdateElementSchema),
   resolver.authorize(),
   async ({ id, ...data }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const element = await db.element.update({ where: { id }, data });
+    const element = await db.element.update({ where: { id }, data })
 
-    return element;
+    return element
   }
-);
+)
--- src/elements/schemas.ts
+++ src/elements/schemas.ts
@@ -2,14 +2,14 @@ import { z } from "zod"
 
 export const FormElementSchema = z.object({
   name: z.string(),
-  description: z.string(),
+  description: z.string().optional().nullable(),
   // template: __fieldName__: z.__zodType__(),
 })
 
 export const CreateElementSchema = z.object({
   name: z.string(),
   projectId: z.number(),
-  description: z.string(),
+  description: z.string().optional().nullable(),
   // template: __fieldName__: z.__zodType__(),
 })
 
