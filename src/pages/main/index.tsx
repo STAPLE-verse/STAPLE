b4c6ef73bceb2ec74d683efffdee50af345c025e
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-08-29 10:41:07 +0200

    factored out widgetobject creation

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,15 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import { Suspense, useEffect } from "react"
-import { Routes } from "@blitzjs/next"
 import Head from "next/head"
 import { useMutation, useQuery } from "@blitzjs/rpc"
 import Layout from "src/core/layouts/Layout"
 import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import getUserWidgets from "src/widgets/queries/getUserWidgets"
-
-// make things draggable
 import React, { useState } from "react"
 import {
   DndContext,
@@ -25,112 +19,48 @@ import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable"
 import updateWidget from "src/widgets/mutations/updateWidget"
 import setWidgets from "src/widgets/mutations/setWidgets"
 import toast from "react-hot-toast"
-import PrimaryLink from "../../core/components/PrimaryLink"
-import {
-  GetProjectDisplay,
-  GetUpcomingTaskDisplay,
-  GetOverdueTaskDisplay,
-  GetNotificationDisplay,
-} from "../../core/components/GetDashboardDisplay"
 import getDashboardTasks from "../../tasks/queries/getDashboardTasks"
 import getDashboardProjects from "src/projects/queries/getDashboardProjects"
 import getLatestUnreadNotifications from "src/notifications/queries/getLatestUnreadNotifications"
+import { WidgetObject, constructWidget } from "src/widgets/utils/constructWidget"
 
 const MainPage = () => {
-  const currentUser = useCurrentUser()
+  // Setup
   const [updateWidgetMutation] = useMutation(updateWidget)
   const [setWidgetMutation] = useMutation(setWidgets)
+
+  const [boxes, setBoxes] = useState<WidgetObject[]>([])
+
+  // Get data
+  // User data
+  const currentUser = useCurrentUser()
+  //Widget content
   const [{ upcomingTasks, pastDueTasks }] = useQuery(getDashboardTasks, undefined)
   const [{ projects }] = useQuery(getDashboardProjects, undefined)
   const [{ notifications }] = useQuery(getLatestUnreadNotifications, {})
-
-  // Get the widgets for the user
-  const [boxes, setBoxes] = useState([])
-
+  // Widgets for the user
   const [fetchedWidgets] = useQuery(getUserWidgets, {
     userId: currentUser?.id,
   })
 
-  // then update them on their screen
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // Construct widgets based on user data
   useEffect(() => {
     if (fetchedWidgets.length > 0) {
       const sortedWidgets = fetchedWidgets.sort((a, b) => a.position - b.position)
       const updatedBoxes = sortedWidgets.map((widget) => {
-        switch (widget.type) {
-          case "LastProject":
-            return {
-              id: widget.id,
-              title: "Last Updated Projects",
-              display: <GetProjectDisplay projects={projects} />,
-              link: <PrimaryLink route={Routes.ProjectsPage()} text="All Projects" />,
-              position: widget.position,
-              size: "col-span-6",
-              tooltipId: "tool-last-project",
-              tooltipContent: "Three recently updated projects",
-            }
-          case "Notifications":
-            return {
-              id: widget.id,
-              title: "Notifications",
-              display: <GetNotificationDisplay notifications={notifications} />,
-              link: <PrimaryLink route={Routes.NotificationsPage()} text="All Tasks" />,
-              position: widget.position,
-              size: "col-span-6",
-              tooltipId: "tool-notifications",
-              tooltipContent: "Three recent notifications for all projects",
-            }
-          case "OverdueTask":
-            return {
-              id: widget.id,
-              title: "Overdue Tasks",
-              display: <GetOverdueTaskDisplay pastDueTasks={pastDueTasks} />,
-              link: <PrimaryLink route={Routes.AllTasksPage()} text="All Notifications" />,
-              position: widget.position,
-              size: "col-span-6",
-              tooltipId: "tool-overdue",
-              tooltipContent: "Three overdue tasks for all projects",
-            }
-          case "UpcomingTask":
-            return {
-              id: widget.id,
-              title: "Upcoming Tasks",
-              display: <GetUpcomingTaskDisplay upcomingTasks={upcomingTasks} />,
-              link: <PrimaryLink route={Routes.AllTasksPage()} text="All Tasks" />,
-              position: widget.position,
-              size: "col-span-6",
-              tooltipId: "tool-upcoming",
-              tooltipContent: "Three upcoming tasks for all projects",
-            }
-          default:
-            return {
-              id: widget.id,
-              title: "Unknown Widget",
-              display: <div>Widget configuration error</div>,
-              link: <div />,
-              position: widget.position,
-              size: "col-span-6",
-              tooltipId: "tool-unknown",
-              tooltipContent: "Unknown widget",
-            }
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        return constructWidget({ widget, projects, notifications, pastDueTasks, upcomingTasks })
       })
       setBoxes(updatedBoxes)
     } else {
-      // console.log("no widgets")
       // Call the mutation
       setWidgetMutation({ id: currentUser?.id })
         .then(() => {
-          //console.log("Widget positions updated successfully")
           toast.success(`Added dashboard, please refresh!`)
         })
-        .catch((error) => {
-          //console.error("Error updating widget positions:", error)
+        .catch(() => {
           toast.error(`Issue with dashboard, please contact help.`)
         })
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchedWidgets])
 
   const handleDragEnd = async (event) => {
@@ -149,12 +79,8 @@ const MainPage = () => {
 
         // Call the mutation
         updateWidgetMutation({ positions: updatedPositions })
-          .then(() => {
-            //console.log("Widget positions updated successfully")
-          })
-          .catch((error) => {
-            //console.error("Error updating widget positions:", error)
-          })
+          .then(() => {})
+          .catch((error) => {})
 
         return newBoxes
       })
@@ -182,7 +108,7 @@ const MainPage = () => {
           </div>
 
           <DndContext
-            collisionDectection={closestCorners}
+            collisionDetection={closestCorners}
             onDragEnd={handleDragEnd}
             sensors={sensors}
           >
---
+++ src/widgets/utils/constructWidget.tsx
@@ -0,0 +1,95 @@
+import PrimaryLink from "src/core/components/PrimaryLink"
+import {
+  GetProjectDisplay,
+  GetUpcomingTaskDisplay,
+  GetOverdueTaskDisplay,
+  GetNotificationDisplay,
+} from "src/core/components/GetDashboardDisplay"
+import { Routes } from "@blitzjs/next"
+import { Project, Widget } from "db"
+import { ReactNode } from "react"
+
+export type WidgetObject = {
+  id: number
+  title: string
+  display: ReactNode
+  link: ReactNode
+  position: number
+  size: string | null
+  tooltipId: string
+  tooltipContent: string
+}
+
+type WidgetComponentProps = {
+  widget: Widget
+  projects: any[]
+  notifications: any[]
+  pastDueTasks: any[]
+  upcomingTasks: any[]
+}
+
+export const constructWidget = ({
+  widget,
+  projects,
+  notifications,
+  pastDueTasks,
+  upcomingTasks,
+}: WidgetComponentProps): WidgetObject => {
+  switch (widget.type) {
+    case "LastProject":
+      return {
+        id: widget.id,
+        title: "Last Updated Projects",
+        display: <GetProjectDisplay projects={projects} />,
+        link: <PrimaryLink route={Routes.ProjectsPage()} text="All Projects" />,
+        position: widget.position,
+        size: "col-span-6",
+        tooltipId: "tool-last-project",
+        tooltipContent: "Three recently updated projects",
+      }
+    case "Notifications":
+      return {
+        id: widget.id,
+        title: "Notifications",
+        display: <GetNotificationDisplay notifications={notifications} />,
+        link: <PrimaryLink route={Routes.NotificationsPage()} text="All Tasks" />,
+        position: widget.position,
+        size: "col-span-6",
+        tooltipId: "tool-notifications",
+        tooltipContent: "Three recent notifications for all projects",
+      }
+    case "OverdueTask":
+      return {
+        id: widget.id,
+        title: "Overdue Tasks",
+        display: <GetOverdueTaskDisplay pastDueTasks={pastDueTasks} />,
+        link: <PrimaryLink route={Routes.AllTasksPage()} text="All Notifications" />,
+        position: widget.position,
+        size: "col-span-6",
+        tooltipId: "tool-overdue",
+        tooltipContent: "Three overdue tasks for all projects",
+      }
+    case "UpcomingTask":
+      return {
+        id: widget.id,
+        title: "Upcoming Tasks",
+        display: <GetUpcomingTaskDisplay upcomingTasks={upcomingTasks} />,
+        link: <PrimaryLink route={Routes.AllTasksPage()} text="All Tasks" />,
+        position: widget.position,
+        size: "col-span-6",
+        tooltipId: "tool-upcoming",
+        tooltipContent: "Three upcoming tasks for all projects",
+      }
+    default:
+      return {
+        id: widget.id,
+        title: "Unknown Widget",
+        display: <div>Widget configuration error</div>,
+        link: <div />,
+        position: widget.position,
+        size: "col-span-6",
+        tooltipId: "tool-unknown",
+        tooltipContent: "Unknown widget",
+      }
+  }
+}
