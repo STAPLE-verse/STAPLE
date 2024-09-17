7923bb4c511a5ad952fcc4b28666351c2d36dd35
The Doom Lab <buchananlab@gmail.com>
2024-09-17 19:59:03 -0400

    all main pages clickable

--- src/tasks/components/AllTaskList.tsx
+++ src/tasks/components/AllTaskList.tsx
@@ -5,51 +5,27 @@ import Table from "src/core/components/Table"
 import { allTasksTableColumns } from "./TaskTable"
 import { processAllTasks } from "../utils/processTasks"
 
+import getTaskLogs from "src/tasklogs/queries/getTaskLogs"
+
 export const AllTasksList = () => {
   const currentUser = useCurrentUser()
 
-  const [{ tasks }] = useQuery(getTasks, {
+  const [taskLogs] = useQuery(getTaskLogs, {
     where: {
-      // Only return tasks where user is assigned
-      OR: [
-        {
-          assignees: {
-            // Individual projectMember
-            some: { projectMember: { user: { id: currentUser?.id } }, teamId: null },
-          },
-        },
-        {
-          assignees: {
-            some: {
-              // ProjectMember as part of a team
-              team: { projectMembers: { some: { user: { id: currentUser?.id } } } },
-              projectMemberId: null,
-            },
-          },
-        },
-      ],
-    },
-    include: {
-      project: true,
-      // Only return assignments of the user
-      assignees: {
-        where: {
-          OR: [
-            {
-              projectMember: { user: { id: currentUser?.id } }, // Individual projectMember
-            },
-            {
-              team: { projectMembers: { some: { user: { id: currentUser?.id } } } }, // ProjectMember as part of a team
-            },
-          ],
-        },
-        include: { statusLogs: { orderBy: { createdAt: "desc" } } },
+      assignedTo: {
+        users: { some: { id: currentUser?.id } },
       },
     },
+    include: {
+      task: true,
+    },
+
     orderBy: { id: "asc" },
   })
 
-  const processedTasks = processAllTasks(tasks)
+  //console.log(taskLogs)
+
+  const processedTasks = processAllTasks(taskLogs)
 
   return (
     <main className="flex flex-col mt-2 mx-auto w-full max-w-7xl">
