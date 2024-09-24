35c7d8830439849c17952d509704f60636a9abc1
The Doom Lab <buchananlab@gmail.com>
2024-09-24 15:22:48 -0400

    add filtering and grab lastest logs

--- src/widgets/components/widgets/ProjectOverdueTasks.tsx
+++ src/widgets/components/widgets/ProjectOverdueTasks.tsx
@@ -9,6 +9,7 @@ import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import moment from "moment"
 import { useQuery } from "@blitzjs/rpc"
 import getTaskLogs from "src/tasklogs/queries/getTaskLogs"
+import getLatestTaskLogs from "src/tasklogs/hooks/getLatestTaskLogs"
 
 type TaskLogWithTask = TaskLog & {
   task: Task
@@ -28,13 +29,23 @@ const ProjectOverdueTasks: React.FC<{ size: "SMALL" | "MEDIUM" | "LARGE" }> = ({
     include: { task: true }, // Include the task relation
   })
   const typedTaskLogs = taskLogs as TaskLogWithTask[]
-  // Filter for overdue tasks
-  const pastDueTasks = typedTaskLogs.filter((taskLog) => {
-    if (taskLog && taskLog.task.deadline) {
-      return moment(taskLog.task.deadline).isBefore(moment(), "minute")
-    }
-    return false
-  })
+
+  // get latest log for each task
+  const latestLog = getLatestTaskLogs(typedTaskLogs)
+
+  // Filter for overdue tasks, incomplete, then sort
+  const pastDueTasks = latestLog
+    .filter((taskLog) => {
+      if (taskLog && taskLog.task.deadline && taskLog.status === Status.NOT_COMPLETED) {
+        return moment(taskLog.task.deadline).isBefore(moment(), "minute")
+      }
+      return false
+    })
+    .sort((a, b) => {
+      // Sort by createdAt in descending order
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    })
+    .slice(0, 3) // top three rows
 
   return (
     <Widget
