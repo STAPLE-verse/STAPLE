eed9494d41a3e405ff17d8c198837940024c6100
The Doom Lab <buchananlab@gmail.com>
2024-09-24 15:30:02 -0400

    update with getlatestlog

--- src/tasks/queries/getTaskStats.ts
+++ src/tasks/queries/getTaskStats.ts
@@ -1,6 +1,7 @@
 import { resolver, useQuery } from "@blitzjs/rpc"
 import db, { Status, MemberPrivileges, TaskLog } from "db"
-import { getLatestTaskLog } from "src/tasklogs/utils/getLatestTaskLog"
+import getLatestTaskLogs from "src/tasklogs/hooks/getLatestTaskLogs"
+import getTaskLogs from "src/tasklogs/queries/getTaskLogs"
 import { z } from "zod"
 
 const GetTaskStatsSchema = z.object({
@@ -32,7 +33,7 @@ export default resolver.pipe(
         where: { projectId: projectId },
       })
 
-      // Completition based on task.status
+      // Completion based on task.status
       const completedTask = await db.task.count({
         where: { projectId: projectId, status: Status.COMPLETED },
       })
@@ -43,20 +44,25 @@ export default resolver.pipe(
       }
     } else if (privilege === MemberPrivileges.CONTRIBUTOR) {
       // If CONTRIBUTOR, return only tasks assigned to the projectMember
-      const allTaskLogs = await getLatestTaskLog(userId, ctx)
+      const [taskLogs] = useQuery(getTaskLogs, {
+        where: {
+          task: { projectId: projectId },
+          assignedTo: { users: { some: { id: userId } } },
+        },
+        include: { task: true },
+      })
 
-      const projectTaskLogs = allTaskLogs
-        .filter((taskLog) => {
-          return taskLog.status === Status.NOT_COMPLETED && taskLog.task.projectId === projectId
-        })
-        .sort((a, b) => {
-          // Sort by createdAt in descending order
-          return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        })
+      // get only the latest log for each task / projectmember
+      const latestTaskLogs = await getLatestTaskLogs(taskLogs)
 
-      const allTask = allTaskLogs.length
+      // get the completed logs
+      const projectTaskLogs = latestTaskLogs.filter((taskLog) => {
+        return taskLog.status === Status.COMPLETED
+      })
 
-      // Completition based on latest assignment status
+      const allTask = latestTaskLogs.length
+
+      // Completion based on latest assignment status
       const completedTask = projectTaskLogs.length
 
       return {
