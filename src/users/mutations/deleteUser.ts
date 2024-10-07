067c0ca6241f2cce08b1b5adc8e5967c71be60a8
The Doom Lab <buchananlab@gmail.com>
2024-10-06 21:37:17 -0500

    typoes and types

--- src/users/mutations/deleteUser.ts
+++ src/users/mutations/deleteUser.ts
@@ -12,17 +12,29 @@ export default resolver.pipe(resolver.authorize(), async (_, ctx) => {
   // TODO: Decide what to do with user information when the profile is deleted
   // Find all projects of the user
   const userProjects = await db.projectMember.findMany({
-    where: { userId: user.id },
-    include: { project: { include: { projectmembers: true } } },
+    where: {
+      users: {
+        some: {
+          id: user.id, // Filter to include only project members that include this user
+        },
+      },
+    },
+    include: {
+      project: {
+        include: {
+          projectMembers: true, // Include all project members
+        },
+      },
+    },
   })
   if (!userProjects) throw new NotFoundError()
 
   userProjects.map(async (userProject) => {
     // Delete projects where there are no other projectmembers
-    if (userProject.project.projectmembers.length === 1) {
+    if (userProject.project.projectMembers.length === 1) {
       await db.project.delete({ where: { id: userProject.projectId } })
       // Keep projects with multiple projectmembers but delete user contribution modal
-    } else if (userProject.project.projectmembers.length > 1) {
+    } else if (userProject.project.projectMembers.length > 1) {
       await db.projectMember.delete({ where: { id: userProject.id } })
     }
   })
