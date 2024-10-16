a9c9719ce506e53386b97bcb9c582b48d7b70b57
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-10-16 13:01:52 +0200

    fixed teamName query issue by searching for project team members only

--- src/teams/queries/getTeamNames.ts
+++ src/teams/queries/getTeamNames.ts
@@ -4,12 +4,13 @@ import { z } from "zod"
 
 const GetTeamNamesSchema = z.object({
   userId: z.number(),
+  projectId: z.number().optional().nullable(),
 })
 
 export default resolver.pipe(
   resolver.zod(GetTeamNamesSchema),
   resolver.authorize(),
-  async ({ userId }) => {
+  async ({ userId, projectId }) => {
     // Fetch all project members where name is not null and the userId belongs to a user in the users array
     const projectMembers = await db.projectMember.findMany({
       where: {
@@ -21,6 +22,10 @@ export default resolver.pipe(
             id: userId, // Check if any user in the users array has the given userId
           },
         },
+        // Add optional projectId filter, only if projectId is provided
+        ...(projectId !== null && {
+          projectId: projectId, // Filter by projectId if it's provided
+        }),
       },
       select: {
         name: true,
