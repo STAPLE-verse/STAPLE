2e8e51f756d16a7fbf4bc38a740321e036b2942d
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-10-22 09:52:47 +0200

    added projectFormTable to the form/tables/

--- src/forms/tables/columns/ProjectFormsTable.tsx
+++ src/forms/tables/columns/ProjectFormsTable.tsx
@@ -1,26 +1,23 @@
 import { createColumnHelper } from "@tanstack/react-table"
 import Link from "next/link"
 import { Routes } from "@blitzjs/next"
-import { FormVersion, Task } from "db"
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import { ProjectFormTableData } from "../processing/processProjectFormsTableData"
 
-export interface TaskWithFormVersion extends Task {
-  formVersion: FormVersion | null
-}
-const columnHelper = createColumnHelper<TaskWithFormVersion>()
+const columnHelper = createColumnHelper<ProjectFormTableData>()
 
 export const projectFormsTableColumns = [
-  columnHelper.accessor("name", {
+  columnHelper.accessor("taskName", {
     cell: (info) => <span>{info.getValue()}</span>,
     header: "Task",
   }),
-  columnHelper.accessor((row) => row.formVersion?.name, {
+  columnHelper.accessor("formName", {
     id: "formVersionSchema",
     cell: (info) => <span>{info.getValue()}</span>,
     header: "Form Required",
   }),
   // Check if the id is correct
-  columnHelper.accessor("id", {
+  columnHelper.accessor("taskId", {
     id: "view",
     enableColumnFilter: false,
     enableSorting: false,
