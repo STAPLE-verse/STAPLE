f9521bc3d6f8db718ae92259d973c2310fafa5d1
The Doom Lab <buchananlab@gmail.com>
2024-10-23 08:30:09 -0500

    fix missing username in this test

--- src/auth/mutations/resetPassword.test.ts
+++ src/auth/mutations/resetPassword.test.ts
@@ -29,6 +29,7 @@ describe("resetPassword mutation", () => {
     const user = await db.user.create({
       data: {
         email: "user@example.com",
+        username: "fakeuser",
         tokens: {
           // Create old token to ensure it's deleted
           create: [
