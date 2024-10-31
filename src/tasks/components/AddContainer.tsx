a33a0df490da8f393f02290f2e05a29af13850c9
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-10-31 08:26:48 +0100

    replaced taskmodal component with the already existing modal component

--- src/tasks/components/AddContainer.tsx
+++ src/tasks/components/AddContainer.tsx
@@ -1,41 +1,52 @@
 import { useMutation } from "@blitzjs/rpc"
 import createColumn from "src/tasks/mutations/createColumn"
 import { useState } from "react"
-import TaskModal from "./TaskModal"
 import TaskInput from "./TaskInput"
 import { Tooltip } from "react-tooltip"
+import Modal from "src/core/components/Modal"
 
 const AddContainer = ({ projectId, refetch }) => {
   const [createColumnMutation] = useMutation(createColumn)
 
   const [showAddContainerModal, setShowAddContainerModal] = useState(false)
+  const handleToggleContainerModal = () => {
+    setShowAddContainerModal((prev) => !prev)
+  }
+
   const [containerName, setContainerName] = useState("")
 
   const onAddContainer = async () => {
     if (!containerName) return
     await createColumnMutation({ projectId: projectId, name: containerName })
     setContainerName("")
-    setShowAddContainerModal(false)
+    handleToggleContainerModal()
     refetch()
   }
 
   return (
     <>
-      <TaskModal showModal={showAddContainerModal} setShowModal={setShowAddContainerModal}>
-        <div className="flex flex-col w-full items-start gap-y-4 bg-base-300">
-          <h1 className="text-3xl font-bold">Add Column</h1>
-          <TaskInput
-            type="text"
-            placeholder="Container Title"
-            name="containername"
-            value={containerName}
-            onChange={(e) => setContainerName(e.target.value)}
-          />
+      <Modal open={showAddContainerModal}>
+        <h1 className="flex justify-center mb-2 text-3xl">Add Column</h1>
+        <TaskInput
+          type="text"
+          placeholder="Container Title"
+          name="containername"
+          value={containerName}
+          onChange={(e) => setContainerName(e.target.value)}
+        />
+        <div className="flex justify-between w-full mt-4">
           <button type="button" className="btn btn-primary" onClick={onAddContainer}>
             Add Column
           </button>
+          <button
+            type="button"
+            className="btn btn-secondary ml-2"
+            onClick={handleToggleContainerModal}
+          >
+            Close
+          </button>
         </div>
-      </TaskModal>
+      </Modal>
 
       <div className="flex items-center justify-between gap-y-2">
         <h1 className="text-3xl font-bold" data-tooltip-id="kanban-tooltip">
