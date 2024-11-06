e81e25bfb19a4680a6ab2b3b1c5bebd3b9945429
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-11-06 19:45:20 +0100

    factored out pagination controls and simplified projectList

--- src/projects/components/ProjectsList.tsx
+++ src/projects/components/ProjectsList.tsx
@@ -1,22 +1,18 @@
-import { Routes } from "@blitzjs/next"
-import Link from "next/link"
 import { usePaginatedQuery } from "@blitzjs/rpc"
-import router from "next/router"
 import getProjects from "src/projects/queries/getProjects"
-import DateFormat from "src/core/components/DateFormat"
+import ProjectCard from "./ProjectCard"
+import PaginationControls from "src/core/components/PaginationControls"
+import { Prisma } from "@prisma/client"
 
 const ITEMS_PER_PAGE = 7
 
 export const ProjectsList = ({ searchTerm, currentUser, page }) => {
-  let where
-  where = {
+  const where: Prisma.ProjectWhereInput = {
     AND: [
       {
         projectMembers: {
           some: {
-            users: {
-              some: { id: currentUser.id },
-            },
+            users: { some: { id: currentUser.id } },
             deleted: false,
             name: null,
           },
@@ -26,14 +22,14 @@ export const ProjectsList = ({ searchTerm, currentUser, page }) => {
         OR: [
           {
             name: {
-              contains: `${searchTerm}`,
-              mode: "insensitive",
+              contains: searchTerm,
+              mode: Prisma.QueryMode.insensitive,
             },
           },
           {
             description: {
-              contains: `${searchTerm}`,
-              mode: "insensitive",
+              contains: searchTerm,
+              mode: Prisma.QueryMode.insensitive,
             },
           },
         ],
@@ -47,47 +43,14 @@ export const ProjectsList = ({ searchTerm, currentUser, page }) => {
     skip: ITEMS_PER_PAGE * page,
     take: ITEMS_PER_PAGE,
   })
-  console.log(projects)
-  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
-  const goToNextPage = () => router.push({ query: { page: page + 1 } })
 
   return (
     <div>
       {projects.map((project) => (
-        <div className="collapse collapse-arrow bg-base-300 mb-2" key={project.id}>
-          {/* Don't change this one it's not a check box */}
-          <input type="checkbox" />
-          <div className="collapse-title text-xl font-medium">{project.name}</div>
-          <div className="collapse-content mb-4">
-            <p className="mb-2">{project.description}</p>
-            <p className="italic mb-2">
-              Last update: <DateFormat date={project.updatedAt}></DateFormat>
-            </p>
-            {/* TODO: Change button position by other method then using absolute */}
-            <div className="justify-end absolute bottom-2 right-6">
-              <Link
-                className="btn btn-primary mb-2"
-                href={Routes.ShowProjectPage({ projectId: project.id })}
-              >
-                Open
-              </Link>
-            </div>
-          </div>
-        </div>
+        <ProjectCard key={project.id} project={project} />
       ))}
       {/* Previous and next page btns */}
-      <div className="join grid grid-cols-2 mt-4">
-        <button
-          className="join-item btn btn-secondary"
-          disabled={page === 0}
-          onClick={goToPreviousPage}
-        >
-          Previous
-        </button>
-        <button className="join-item btn btn-secondary" disabled={!hasMore} onClick={goToNextPage}>
-          Next
-        </button>
-      </div>
+      <PaginationControls page={page} hasMore={hasMore} />
     </div>
   )
 }
