f12114eaa034592d576bea0ad68526d3828823c8
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-11-17 19:18:20 +0100

    simplified taskLogCompletition logic because of new projectmember

--- src/tasklogs/components/TaskLogCompletion.tsx
+++ src/tasklogs/components/TaskLogCompletion.tsx
@@ -8,24 +8,18 @@ import TaskLogHistoryModal from "./TaskLogHistoryModal"
 import { useSeparateProjectMembers } from "src/projectmembers/hooks/useSeparateProjectMembers"
 import { filterLatestTaskLog } from "../utils/filterLatestTaskLog"
 import Card from "src/core/components/Card"
+import { CompleteRow } from "./CompleteRow"
 
 export const TaskLogCompletion = () => {
   const { task, projectMembers } = useTaskContext()
-  const { individualProjectMembers, teamProjectMembers } =
-    useSeparateProjectMembers<ProjectMemberWithTaskLog>(projectMembers)
   const { projectMember: currentContributor } = useCurrentContributor(task.projectId)
   const currentUser = useCurrentUser()
 
   // Filter based on the currentUser
-  const individualProjectMember = individualProjectMembers.filter((projectMember) =>
-    projectMember.users.some((user) => user.id === currentUser?.id)
-  )[0]
-
-  const filteredTeamProjectMembers = teamProjectMembers.filter(
+  const filteredProjectMembers = projectMembers.filter(
     (projectMember) =>
-      !projectMember.deleted && // Filter out deleted teams
-      // Find assignments of the current user to the task
-      projectMember.users.some((user) => user.id === currentUser?.id)
+      !projectMember.deleted && // Exclude deleted members
+      projectMember.users.some((user) => user.id === currentUser?.id) // Include ProjectMembers that have the current user
   )
 
   return (
@@ -34,84 +28,17 @@ export const TaskLogCompletion = () => {
       tooltipContent="Complete your individual or team task and view completion history"
       className="w-2/3"
     >
-      {/* if no schema complete task as a Individual*/}
-      {!task.formVersion && individualProjectMember && (
-        <div className="flex grid-col-2">
-          <CompleteToggle
-            taskLog={filterLatestTaskLog(individualProjectMember.taskLogAssignedTo)}
-            completedRole="Completed"
-            completedById={currentContributor?.id}
-            completedAs={CompletedAs.INDIVIDUAL}
-          />
-          <span className="mx-2">
-            <TaskLogHistoryModal taskLogs={individualProjectMember.taskLogAssignedTo} />
-          </span>
-        </div>
-      )}
-
-      {/* if no schema complete task as a Team*/}
-      {!task.formVersion && filteredTeamProjectMembers && filteredTeamProjectMembers.length > 0 && (
-        <div>
-          {filteredTeamProjectMembers.map((teamProjectMember) => (
-            <div key={teamProjectMember.id} className="flex flex-row gap-2">
-              <CompleteToggle
-                taskLog={filterLatestTaskLog(teamProjectMember.taskLogAssignedTo)}
-                completedRole="Completed"
-                completedById={currentContributor?.id}
-                completedAs={CompletedAs.TEAM}
-              />
-              <span className="mx-2">
-                <TaskLogHistoryModal taskLogs={teamProjectMember.taskLogAssignedTo} />
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
-
-      {/* if schema and individual*/}
-      {task.formVersion && individualProjectMember && (
-        <div className="flex grid-col-2">
-          <CompleteSchema
-            taskLog={filterLatestTaskLog(individualProjectMember.taskLogAssignedTo)}
-            completedById={currentContributor?.id}
-            completedAs={CompletedAs.INDIVIDUAL}
-            schema={task.formVersion.schema}
-            ui={task.formVersion.uiSchema}
-          />
-          <span className="mx-2">
-            <TaskLogHistoryModal
-              taskLogs={individualProjectMember.taskLogAssignedTo}
-              schema={task.formVersion.schema}
-              ui={task.formVersion.uiSchema}
-            />
-          </span>
-        </div>
-      )}
-
-      {/* if schema and team*/}
-      {task.formVersion && filteredTeamProjectMembers && filteredTeamProjectMembers.length > 0 && (
-        <div className="">
-          {filteredTeamProjectMembers.map((teamProjectMember) => (
-            <div key={teamProjectMember.id} className="mb-2 flex grid-col-2">
-              <CompleteSchema
-                taskLog={filterLatestTaskLog(teamProjectMember.taskLogAssignedTo)}
-                completedById={currentContributor?.id}
-                completedAs={CompletedAs.TEAM}
-                schema={task.formVersion?.schema}
-                ui={task.formVersion?.uiSchema}
-              />
-
-              <span className="mx-2">
-                <TaskLogHistoryModal
-                  taskLogs={teamProjectMember.taskLogAssignedTo}
-                  schema={task.formVersion?.schema}
-                  ui={task.formVersion?.uiSchema}
-                />
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
+      {filteredProjectMembers.map((projectMember) => (
+        <CompleteRow
+          key={projectMember.id}
+          taskLogs={projectMember.taskLogAssignedTo}
+          completedById={currentContributor?.id}
+          completedAs={projectMember.name === null ? CompletedAs.INDIVIDUAL : CompletedAs.TEAM}
+          schema={task.formVersion?.schema}
+          ui={task.formVersion?.uiSchema}
+          isSchema={!!task.formVersion}
+        />
+      ))}
     </Card>
   )
 }
