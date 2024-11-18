87276ef125fbba97bcbc6529eab4aa2a1646c41e
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-11-18 07:42:19 +0100

    took out not used taskId prop from AddRoleForm

--- src/roles/components/AddRoleForm.tsx
+++ src/roles/components/AddRoleForm.tsx
@@ -7,11 +7,10 @@ import getProjectManagerUserIds from "src/projectmembers/queries/getProjectManag
 interface AddRoleFormProps<S extends z.ZodType<any, any>> extends FormProps<S> {
   projectId?: number
   type?: string
-  tasksId?: number[]
 }
 
 export function AddRoleForm<S extends z.ZodType<any, any>>(props: AddRoleFormProps<S>) {
-  const { projectId, type, tasksId, ...formProps } = props
+  const { projectId, type, ...formProps } = props
 
   // Get all roles from all PMs
   const [projectManagerUserIds] = useQuery(getProjectManagerUserIds, {
--- src/roles/components/AddRoleModal.tsx
+++ src/roles/components/AddRoleModal.tsx
@@ -55,7 +55,7 @@ export const AddRoleModal = ({ rows, projectId, refetch, type }) => {
         Add Role
       </button>
       <Modal open={openModal} size="w-7/8 max-w-xl">
-        <div className="">
+        <div>
           <h1 className="flex justify-center mb-2 text-3xl">Add Roles</h1>
           <div className="flex justify-start mt-4">
             <AddRoleForm
