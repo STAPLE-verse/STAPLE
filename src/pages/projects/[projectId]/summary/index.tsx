9e5b8670a9ae9d2c199f876c26407bb243e870e9
The Doom Lab <buchananlab@gmail.com>
2024-11-25 18:43:16 -0500

    simplify page for new information

--- src/pages/projects/[projectId]/summary/index.tsx
+++ src/pages/projects/[projectId]/summary/index.tsx
@@ -1,16 +1,10 @@
 import { Suspense, useState } from "react"
 import { Routes } from "@blitzjs/next"
-import Head from "next/head"
 import Link from "next/link"
 import { useQuery } from "@blitzjs/rpc"
 import { useParam } from "@blitzjs/next"
 import Layout from "src/core/layouts/Layout"
 import getProject from "src/projects/queries/getProject"
-import ByProjectMembers from "src/summary/components/ByProjectMembers"
-import ByTasks from "src/summary/components/ByTasks"
-import ByRoles from "src/summary/components/ByRoles"
-import ByDate from "src/summary/components/ByDate"
-import ByElements from "src/summary/components/ByElements"
 import getRoles from "src/roles/queries/getRoles"
 import getElements from "src/elements/queries/getElements"
 import getProjectMembers from "src/projectmembers/queries/getProjectMembers"
@@ -22,97 +16,10 @@ import getProjectManagers from "src/projectmembers/queries/getProjectManagers"
 import DownloadJSON from "src/forms/components/DownloadJSON"
 
 const Summary = () => {
-  // Setup
-  const [selectedOrganization, setSelectedOrganization] = useState("none")
-
   // Get data
   // Get projects
   const projectId = useParam("projectId", "number")
   const [project] = useQuery(getProject, { id: projectId })
-
-  // Get taskLogs and tasks
-  const [taskLogs] = useQuery(getTaskLogs, {
-    where: {
-      task: {
-        projectId: projectId,
-      },
-    },
-    orderBy: { createdAt: "desc" },
-    include: {
-      task: {
-        include: {
-          roles: true, // Include the roles relation within the task relation
-        },
-      },
-    },
-  })
-
-  // Get elements
-  const [{ elements }] = useQuery(getElements, {
-    where: { project: { id: projectId! } },
-    orderBy: { id: "asc" },
-    // include: { task: true },
-  })
-
-  // Get only contributors one person no team
-  const [{ projectMembers }] = useQuery(getProjectMembers, {
-    where: {
-      projectId: projectId,
-      users: {
-        every: {
-          id: { not: undefined }, // Ensures there's at least one user
-        },
-      },
-      name: { equals: null }, // Ensures the name in ProjectMember is null
-    },
-    include: {
-      users: true,
-      roles: true,
-    },
-  })
-
-  // Map through projectMembers and flatten the users array to extract firstName and lastName
-  const flattenedMembers = projectMembers.map((member) => {
-    // Assuming there's only one user in each projectMember's `users` array
-    const user = member["users"][0] // Safe because you ensured only one user
-    return {
-      projectMemberId: member.id,
-      firstName: user?.firstName ?? "", // Ensure user exists and has firstName
-      lastName: user?.lastName ?? "",
-      role: member["roles"].map((role) => role.name).join(", "), // Flatten roles if needed
-    }
-  })
-
-  // get teams with a name because they can be one person teams
-  const [teams] = useQuery(getProjectMembers, {
-    where: {
-      projectId: projectId,
-      name: { not: null }, // Ensures the name in ProjectMember is non-null
-      users: {
-        some: { id: { not: undefined } }, // Ensures there's at least one user
-      },
-    },
-    include: {
-      users: true,
-      roles: true,
-    },
-  })
-
-  const [projectManagers] = useQuery(getProjectManagers, {
-    projectId: projectId!,
-  })
-
-  const roles = useQuery(getRoles, {
-    where: {
-      userId: { in: projectManagers.map((pm) => pm.userId) }, // Ensure userId is an array
-    },
-  })
-
-  // Handle events
-  const handleOrganizationChanged = (e) => {
-    //do query based on organization
-    setSelectedOrganization(e)
-  }
 
   return (
     <main className="flex flex-col mt-2 mx-auto w-full max-w-7xl">
