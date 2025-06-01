795f1875c6cf42d5fdce394240bf3d7d78990c67
The Doom Lab <buchananlab@gmail.com>
2025-06-01 14:08:17 -0500

    fixing milestone page

--- src/milestones/components/MilestoneInformation.tsx
+++ src/milestones/components/MilestoneInformation.tsx
@@ -1,102 +1,50 @@
-import { Routes } from "@blitzjs/next"
-import { useQuery } from "@blitzjs/rpc"
-import Link from "next/link"
 import Table from "src/core/components/Table"
-import getTasks from "src/tasks/queries/getTasks"
 import { Milestone } from "@prisma/client"
 import DateFormat from "src/core/components/DateFormat"
-import { useState } from "react"
-import UpdateTasks from "./UpdateTasksMilestone"
 import { MilestoneTasksColumns } from "../tables/columns/MilestoneTasksColumns"
-import { processMilestoneTasks } from "../tables/processing/processMilestoneTasks"
 import TooltipWrapper from "src/core/components/TooltipWrapper"
+import CollapseCard from "src/core/components/CollapseCard"
+import { MilestoneTasksData } from "../tables/processing/processMilestoneTasks"
+import { MilestoneSummary } from "./MilestoneSummary"
 
 interface MilestoneInformationProps {
   milestone: Milestone
-  projectId: number | undefined
-  onTasksUpdated: () => void
+  processedTasks: MilestoneTasksData[]
+  projectId: number
 }
 
 export const MilestoneInformation: React.FC<MilestoneInformationProps> = ({
   milestone,
+  processedTasks,
   projectId,
-  onTasksUpdated,
 }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const openModal = () => setIsModalOpen(true)
-  const closeModal = () => setIsModalOpen(false)
-
-  // Get tasks
-  const [{ tasks }, { refetch }] = useQuery(getTasks, {
-    where: {
-      project: { id: projectId! },
-    },
-    orderBy: { id: "asc" },
-  })
-
-  const milestoneTasks = tasks.filter((task) => task.milestoneId === milestone.id)
-
-  const processedTasks = processMilestoneTasks(milestoneTasks)
-
   return (
-    <div className="flex flex-row justify-center w-full">
-      <div className="card bg-base-300 w-1/3 mr-2">
-        <div className="card-body">
-          {/* Milestone name */}
-          <div className="card-title" data-tooltip-id="milestone-tool">
-            {milestone.name}
-          </div>
-          <TooltipWrapper
-            id="milestone-tool"
-            content="Overall milestone information"
-            className="z-[1099] ourtooltips"
-          />
-          {/* Milestone description */}
-          {milestone.description}
-          {/* Milestone last update */}
-          <p className="italic">
-            Last update: <DateFormat date={milestone.updatedAt}></DateFormat>
-          </p>
-          {/* Show update milestone page */}
-          <div className="card-actions justify-end">
-            <Link
-              className="btn btn-primary"
-              href={Routes.EditMilestonePage({ projectId: projectId!, milestoneId: milestone.id })}
-            >
-              Update Milestone
-            </Link>
-
-            <button className="btn btn-secondary" onClick={openModal}>
-              Update Tasks
-            </button>
-            <UpdateTasks
-              milestoneId={milestone.id}
-              open={isModalOpen}
-              onClose={closeModal}
-              onTasksUpdated={refetch}
-              tasks={tasks}
-            />
-          </div>
+    <>
+      <CollapseCard title="Milestone Information" defaultOpen={true}>
+        {/* Milestone name */}
+        <div className="card-title" data-tooltip-id="milestone-tool">
+          {milestone.name}
         </div>
-      </div>
+        <TooltipWrapper
+          id="milestone-tool"
+          content="Overall milestone information"
+          className="z-[1099] ourtooltips"
+        />
+        {/* Milestone description */}
+        {milestone.description}
+        {/* Milestone last update */}
+        <p className="italic">
+          Last update: <DateFormat date={milestone.updatedAt}></DateFormat>
+        </p>
+      </CollapseCard>
 
-      {/* Tasks */}
-      <div className="card bg-base-300 w-2/3 h-auto">
-        <div className="card-body">
-          <div className="card-title" data-tooltip-id="tasks-tool">
-            Tasks
-          </div>
-          <TooltipWrapper
-            id="tasks-tool"
-            content="Tasks assigned to this milestone"
-            className="z-[1099] ourtooltips"
-          />
-          <div className="overflow-x-auto">
-            <Table columns={MilestoneTasksColumns} data={processedTasks} addPagination={true} />
-          </div>
+      <MilestoneSummary milestone={milestone} projectId={projectId} />
+
+      <CollapseCard title="Tasks" className="mt-4">
+        <div className="overflow-x-auto">
+          <Table columns={MilestoneTasksColumns} data={processedTasks} addPagination={true} />
         </div>
-      </div>
-    </div>
+      </CollapseCard>
+    </>
   )
 }
--- src/pages/projects/[projectId]/milestones/[milestoneId].tsx
+++ src/pages/projects/[projectId]/milestones/[milestoneId].tsx
@@ -1,13 +1,21 @@
-import { Suspense } from "react"
 import Layout from "src/core/layouts/Layout"
 import getMilestone from "src/milestones/queries/getMilestone"
-import { useQuery } from "@blitzjs/rpc"
-import { useParam } from "@blitzjs/next"
+import { useMutation, useQuery } from "@blitzjs/rpc"
+import { Routes, useParam } from "@blitzjs/next"
 import useProjectMemberAuthorization from "src/projectprivileges/hooks/UseProjectMemberAuthorization"
 import { MemberPrivileges } from "db"
 import { MilestoneInformation } from "src/milestones/components/MilestoneInformation"
 import { useMemberPrivileges } from "src/projectprivileges/components/MemberPrivilegesContext"
-import { MilestoneSummary } from "src/milestones/components/MilestoneSummary"
+import { InformationCircleIcon } from "@heroicons/react/24/outline"
+import { Tooltip } from "react-tooltip"
+import Link from "next/link"
+import UpdateTasksMilestone from "src/milestones/components/UpdateTasksMilestone"
+import { useState } from "react"
+import { processMilestoneTasks } from "src/milestones/tables/processing/processMilestoneTasks"
+import type { MilestoneTasksData } from "src/milestones/tables/processing/processMilestoneTasks"
+import getTasks from "src/tasks/queries/getTasks"
+import router from "next/router"
+import deleteMilestone from "src/milestones/mutations/deleteMilestone"
 
 const ShowMilestonePage = () => {
   // ProjectMember authentication
@@ -17,23 +25,79 @@ const ShowMilestonePage = () => {
   // Get milestones
   const projectId = useParam("projectId", "number")
   const milestoneId = useParam("milestoneId", "number")
-  const [milestone, { refetch }] = useQuery(getMilestone, { id: milestoneId })
+  const [milestone, { refetch: refetchMilestone }] = useQuery(getMilestone, { id: milestoneId })
+  const [deleteMilestoneMutation] = useMutation(deleteMilestone)
+  // Delete event
+  const handleDelete = async () => {
+    if (window.confirm("This milestone will be deleted. Is that ok?")) {
+      await deleteMilestoneMutation({ id: milestone.id })
+      await router.push(Routes.MilestonesPage({ projectId: projectId! }))
+    }
+  }
+
+  // for update tasks
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+  const [{ tasks }, { refetch: refetchTasks }] = useQuery(getTasks, {
+    where: {
+      project: { id: projectId! },
+    },
+    orderBy: { id: "asc" },
+  })
+  const milestoneTasks = tasks.filter((task) => task.milestoneId === milestone.id)
+  const processedTasks = processMilestoneTasks(milestoneTasks)
+
+  console.log(processedTasks)
 
   return (
     // @ts-expect-error children are clearly passed below
     <Layout title="Milestone Page">
       <main className="flex flex-col mb-2 mt-2 mx-auto w-full max-w-7xl">
+        <h1 className="flex justify-center items-center text-3xl">
+          Milestone:{" "}
+          <span className="italic ml-2">
+            {milestone.name.length > 50 ? milestone.name.slice(0, 50) + "…" : milestone.name}
+          </span>
+          <InformationCircleIcon
+            className="ml-2 h-5 w-5 stroke-2 text-info"
+            data-tooltip-id="team-tooltip"
+          />
+          <Tooltip
+            id="team-tooltip"
+            content="This page allows you to review the contributors information, overall sttatistics, and tasks they are assigned to as an individual."
+            className="z-[1099] ourtooltips"
+          />
+        </h1>
+        <div className="gap-2 justify-center items-center flex m-4">
+          <Link
+            className="btn btn-primary"
+            href={Routes.EditMilestonePage({ projectId: projectId!, milestoneId: milestone.id })}
+          >
+            Update Milestone
+          </Link>
+
+          <button className="btn btn-secondary" onClick={openModal}>
+            Update Tasks
+          </button>
+          <UpdateTasksMilestone
+            milestoneId={milestone.id}
+            open={isModalOpen}
+            onClose={closeModal}
+            onTasksUpdated={refetchTasks}
+            tasks={tasks}
+          />
+
+          <button type="button" className="btn btn-warning" onClick={handleDelete}>
+            Delete Milestone
+          </button>
+        </div>
         <div>
-          <Suspense fallback={<div>Loading...</div>}>
-            <MilestoneInformation
-              milestone={milestone}
-              projectId={projectId}
-              onTasksUpdated={refetch}
-            />
-            {privilege == MemberPrivileges.PROJECT_MANAGER && (
-              <MilestoneSummary milestone={milestone} projectId={projectId} />
-            )}
-          </Suspense>
+          <MilestoneInformation
+            milestone={milestone}
+            processedTasks={processedTasks}
+            projectId={projectId!}
+          />
         </div>
       </main>
     </Layout>
