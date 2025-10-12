a231e6aed354e1d797926f71ab72a442dc70279f
The Doom Lab <buchananlab@gmail.com>
2025-10-12 18:17:10 -0500

    fixing the scrolling with collapse cards

--- src/tasks/components/TaskSummary.tsx
+++ src/tasks/components/TaskSummary.tsx
@@ -6,7 +6,7 @@ import { TaskLogTable } from "./TaskLogTable"
 // Create task summary for the PM
 export const TaskSummary = ({ contributorFilter }: { contributorFilter?: number }) => {
   return (
-    <div className="flex flex-row justify-center mt-2 mb-2">
+    <div className="flex flex-row justify-center mt-4 mb-2">
       {/* overall project information */}
       <CollapseCard title="Task Completion" className="w-full" defaultOpen={true}>
         {!contributorFilter && (
