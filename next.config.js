dd75dc5286f5224f018d049f33a48f7e56a1bb24
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2026-07-26 23:04:30 +0200

    added form-studi shared staple-verse json form handling package to the app

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const { withBlitz } = require("@blitzjs/next")
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
  **/
-const config = {}
+const config = {
+  transpilePackages: ["@staple-verse/form-studio"],
+}
 
 module.exports = withBlitz(config)
